025449ecae8537c3430db4f443a074c3491391b4
Kumo <hsumyatmon.ntu@gmail.com>
2019-08-09 10:45:13 +0630

    revenue container mmh

--- src/features/revenue/containers/RevenueContainer.js
+++ src/features/revenue/containers/RevenueContainer.js
@@ -2,7 +2,27 @@ import React from "react"
 
 const RevenueContainer = props => {
     return (
-        <div>RevenueContainer</div>
+        <div className="container-fluid">
+            <div className="row px-1">
+                <div className="p-1 bg-white w-100">revenue card</div>
+            </div>
+
+
+
+            <div className="row pt-2">
+                <div className="col-md-6 p-1">
+                    <div className="bg-white " style={{ height: 300 }}>
+
+                    </div>
+                </div>
+
+                <div className="col-md-6 p-1">
+                    <div className="bg-white" style={{ height: 300 }}>
+
+                    </div>
+                </div>
+            </div>
+        </div >
     )
 }
 

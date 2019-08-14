54f38bbd12ba4f431b59be7c549dbdcf9693a89a
Kumo <hsumyatmon.ntu@gmail.com>
2019-08-14 10:35:30 +0630

    kmtable

--- src/features/home/containers/GlobalListContainer.js
+++ src/features/home/containers/GlobalListContainer.js
@@ -1,23 +1,22 @@
-import React, { useState } from "react"
-import { Link } from "react-router-dom"
+import React from "react"
 import * as route from "../../../config/route.config"
 import { dec } from "../../../network-sec/cypher"
-import DataTable, { memoize } from 'react-data-table-component';
+import { memoize } from 'react-data-table-component';
 import tableTheme from "./tableTheme"
 
 import { withMedia } from 'react-media-query-hoc'
 import { fsc } from "../../../helper/fontColorHelper";
+import KmTable from '../../../kumocom/KmTable'
 
 const GlobalListContainer = props => {
     const { location, media } = props
-    const [page, setPage] = useState(1)
     // const queryData = dec(querystring.parse(location.search).had)
     const handleClick = () => props.history.push(`/${route.site}/${1}${location.search}`)
     return (
         <div className="py-2">
             <div className="">
                 <div className="py-2" style={{}}>
-                    <DataTable
+                    <KmTable
                         columns={columns(media, handleClick)}
                         data={data}
                         keyField={"id"}
@@ -27,10 +26,9 @@ const GlobalListContainer = props => {
                         customTheme={tableTheme(media)}
                         pagination={true}
                         paginationDefaultPage={1}
-                        paginationComponentOptions={{ rowsPerPageText: 'Rows per page:', rangeSeparatorText: 'on' }}
                         paginationTotalRows={data.length}
                         paginationPerPage={3}
-                        paginationComponent={props => <CustomPagination media={media} setPage={setPage} {...props} />}
+                        customPagination={true}
                     />
                 </div>
             </div>
@@ -39,40 +37,6 @@ const GlobalListContainer = props => {
 }
 
 export default withMedia(GlobalListContainer)
-
-const CustomPagination = props => {
-    const { media, setPage, onChangePage, currentPage, rowCount, rowsPerPage, onChangeRowsPerPage } = props
-    const last = Math.ceil(rowCount / rowsPerPage);
-    const left = currentPage > 1 ? currentPage - 1 : 0;
-    const right = currentPage < last ? currentPage + 1 : 0;
-
-    const pages = new Array(last).fill(null).map((v, k) => k + 1)
-    const pageList = pages < 6 ? [...pages] : pages.reduce((r, c, i, a) => {
-        if (c === 1 || c === left || c === currentPage || c === right || c === last) return [...r, c]
-        else if (r[r.length - 1] !== 0) return [...r, 0]
-        else return [...r]
-    }, [])
-
-    const pageView = pageList.map((v, k) => {
-        const { bgColor, txtColor } = currentPage === v ? { bgColor: "#153784", txtColor: "#ffffff" } : { bgColor: "#ffffff", txtColor: "#454545" }
-        const nextPage = v === 1 ? 1
-            : v === last ? last
-                : v === left ? left
-                    : v === right ? right : currentPage
-        return (
-            <div key={k} onClick={() => onChangePage(nextPage)} className="px-1" style={{ cursor: "pointer" }} >
-                {v === 0 && <span style={{ color: "#878787" }}><i className="fas fa-ellipsis-h"></i></span>}
-                {v > 0 && <span className="p-2" style={{ fontSize: 11, color: txtColor, backgroundColor: bgColor, borderRadius: 4 }} >{v}</span>}
-            </div>
-        )
-    })
-
-    return (
-        <div className={`w-100 d-flex align-items-center ${media.mobile ? 'justify-content-start' : 'justify-content-center'}`} style={{ }}>
-            {pageView}
-        </div>
-    )
-}
 
 const data = [
     { id: 1, site: '01 Conan the Barbarian', capacity: '1981', currentOutput: 23, efficiency: "Site1", siteStatus: "col21", more: "MORE INFO", },
@@ -96,6 +60,7 @@ const data = [
     { id: 19, site: '19 Conan the Barbarian', capacity: '1983', currentOutput: 43, efficiency: "Site3", siteStatus: "col21", more: "MORE INFO", },
 
 ];
+
 const columns = memoize((media, handleClick) => [
     {
         name: 'SITE',
--- src/features/maintenance/components/MaintenanceItemCardView.js
+++ src/features/maintenance/components/MaintenanceItemCardView.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import moment from 'moment'
 
 const MaintenanceItemCardView = props => {
     const { } = props
+    const time = moment().format('hh:mm A')
+    const date = moment().format('DD')
+    const month = moment().format('MMM')
+    const Supplier = 'Huakai Farmer Association'
+    const MaintenanceType = 'Ad-Hoc'
+    const Description = 'Database and Server Maintenance'
+    const Width = 280
+    const Edit = () => {
+        alert('You Click Edit')
+    }
     return (
-        <div>MaintenanceItemCardView</div>
+        <div>
+
+        </div>
     )
 }
 

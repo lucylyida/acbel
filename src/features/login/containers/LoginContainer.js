16f1d809ed1e3519e32e97d42845de8ebe13f11b
Kumo <hsumyatmon.ntu@gmail.com>
2019-09-11 18:02:45 +0630

    update login container

--- src/features/login/containers/LoginContainer.js
+++ src/features/login/containers/LoginContainer.js
@@ -8,21 +8,21 @@ import * as route from '../../../config/route.config'
 const Login = props => {
 
     const { media } = props
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('admin@gmail.com')
+    const [password, setPassword] = useState('admin@1234')
     const [show, setShow] = useState(false)
     return (
-        <div className="container-fluid" >
+        <div className="container-fluid pt-5" >
             {/* <div className="row pl-3">
                 <div className="col login-nav py-4" style={{ height: 80, fontSize: font.heading2, }}>
                     <img src={`logo.${theme}.png`} alt="logo" height={25} />
                 </div>
             </div> */}
-            <div className="row justify-content-center align-items-center pb-4" >
+            <div className="row justify-content-center align-items-center py-4" >
                 <div className="col-sm-12 col-md-10 col-lg-6 col-xl-4 py-4">
-                    {/* <div className="row justify-content-center" style={{ color: '#ffffff', fontSize: font.heading1 }}>
+                    <div className="row justify-content-center" style={{ color: '#cccccc', fontSize: 48 }}>
                         <i className="fas fa-lock"></i>
-                    </div> */}
+                    </div>
                     <div className="row justify-content-center align-items-center py-5"
                         style={{ fontSize: fsc(media, 26), color: '#000000', fontWeight: 600 }}>Login To Your Account</div>
                     {/* <LoadingView isLoading={isLoading} /> */}

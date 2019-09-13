e73aa361e2d22a48fe247cb6fcefd74b75b122b7
Nieruht <ethical00002020@gmail.com>
2019-09-13 19:03:22 +0630

    ThureinPush

--- src/features/home/containers/GlobalMapContainer.js
+++ src/features/home/containers/GlobalMapContainer.js
@@ -6,40 +6,41 @@ import MapView from './Map';
 
 import { getvendorfromapi } from '../../../action'
 import { useSelector, useDispatch } from 'react-redux'
-import  * as Action from '../../../action'
+import * as Action from '../../../action'
 
 const GlobalMapContainer = props => {
     const state = useSelector(state => state.vendorReducer)
     const dispatch = useDispatch()
 
-    const { vendorListRaw, siteListRaw } = state
-    const clientLists = siteListRaw.reduce((r,c)=> {
-        const index = r.reduce((r1,c1,i1) => c1.country===c.country && c1.city===c.city ? i1 : r1 , -1)
-        if(r.length===0 || index===-1) {
-            const sites = siteListRaw.filter( site => site.country===c.country && site.city===c.city)
-                .map(site => ({ 
+    const { vendorListRaw, siteNameList } = state
+    
+    const clientLists = siteNameList.reduce((r, c) => {
+        const index = r.reduce((r1, c1, i1) => c1.country === c.country && c1.city === c.city ? i1 : r1, -1)
+        if (r.length === 0 || index === -1) {
+            const sites = siteNameList.filter(site => site.country === c.country && site.city === c.city)
+                .map(site => ({
                     ...site,
-                    name: site.site_name, 
-                    lat: site.latitude.trim()==="" ? 0 : parseFloat(site.latitude), 
-                    lng: site.longitude.trim()==="" ? 0 : parseFloat(site.longitude)
-                 })) // provide the site format to use in Map
-            const latList = sites.map(s =>s.lat)
+                    name: site.site_name,
+                    lat: site.latitude.trim() === "" ? 0 : parseFloat(site.latitude),
+                    lng: site.longitude.trim() === "" ? 0 : parseFloat(site.longitude)
+                })) // provide the site format to use in Map
+            const latList = sites.map(s => s.lat)
             const lngList = sites.map(s => s.lng)
             latList.sort()
             lngList.sort()
-         
+
             const client = ({
                 country: c.country,
                 city: c.city,
-                lat: latList.length===0 ? 0 : latList[0], //: latList[latList.length/2], //first get median and another mean
-                lng: lngList.length===0 ? 0 : lngList[0], //: lngList[lngList.length/2] , //first get median and another mean,
+                lat: latList.length === 0 ? 0 : latList[0], //: latList[latList.length/2], //first get median and another mean
+                lng: lngList.length === 0 ? 0 : lngList[0], //: lngList[lngList.length/2] , //first get median and another mean,
                 sites
             })
+            
             return [...r, client]
         } else return r
     }, [])
-    // console.log({ clientLists })
-   
+
 
     return (
         <div className="mt-3">
--- src/features/home/containers/Map.js
+++ src/features/home/containers/Map.js
@@ -8,12 +8,13 @@ import SolarPanelIcon from "../../../assets/images/solarPanel.png";
 import { withMedia } from 'react-media-query-hoc';
 import * as route from '../../../config/route.config';
 import { withRouter } from 'react-router-dom'
+import equal from "deep-equal";
 
 export class MapContainer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            zoom: 6,
+            zoom: 5,
             stores: [],
             isClientToShow: false,
             showingInfoWindow: false,
@@ -22,11 +23,13 @@ export class MapContainer extends Component {
             isShowInfoWindow: false,
             initialCenter: { lat: 20.444, lng: 96.176 },
             markerPosition: {},
-            infoSiteData : {},
+            infoSiteData: {},
         }
         this.mapRef = React.createRef()
     }
     componentDidMount() {
+
+        // console.log("Did")
         const { clientLists } = this.props
         // console.log({ mapRef: this.mapRef.current })
         const mmap = this.mapRef.current
@@ -38,9 +41,11 @@ export class MapContainer extends Component {
         mmap.map.addListener("bounds_changed", () => {
             const mapBound = mmap.map.getBounds()
             // console.log({ contain: mapBound.contains({ lat: 21.444, lng: 96.176 }) })
+            // console.log(clientLists)
             const clientLocs = clientLists.filter(v => mapBound.contains({ lat: v.lat, lng: v.lng }))
-            // console.log({ clientLocs })
+            // console.log(clientLocs)
             const clientLocs2 = clientLocs.length >= 1 ? clientLocs : clientLists
+            // console.log(clientLocs2)
             const listToShow = clientLocs.length > 1 ? clientLocs : clientLocs2.reduce((r, c) => {
                 const sitesLocs = c.sites.map(v => v)
                 return [...r, ...sitesLocs]
@@ -48,28 +53,61 @@ export class MapContainer extends Component {
             this.setState({ stores: listToShow, isClientToShow: clientLocs.length > 1, isShowInfoWindow: clientLocs.lenght >= 1 })
         })
     }
-    _markerDisplay = () => {
-        const { stores, isClientToShow } = this.state
-        const mmap = this.mapRef.current
 
-        return stores === undefined ? [] : stores.map((store, index) => {
-            const icon = !isClientToShow ? SolarPanelIcon : Animatedicon(this.props)
-            return < Marker
-                label={isClientToShow ? { text: `${store.sites.length}`, color: 'white' } : null}
-                // title={{ text: `${store.sites.length}`, color: 'red' }}
-                icon={icon}
-                key={index}
-                id={index}
-                position={{
-                    lat: store.lat,
-                    lng: store.lng,
-                }}
-                onClick={(props, marker, e) => {
-                    return !isClientToShow ? this._onMarkerClick(props ,marker, e, store) : this._ClientSites(props, marker, e)
-                }}
-            >
-            </Marker>
-        })
+
+
+    _markerDisplay = () => {
+
+
+        const { stores, isClientToShow } = this.state
+        const { clientLists } = this.props
+        console.log(clientLists)
+        const mmap = this.mapRef.current
+        const icon = !isClientToShow ? SolarPanelIcon : Animatedicon(this.props)
+
+        return stores === undefined ? [] : isClientToShow ?
+
+            clientLists.map((store, index) => {
+                console.log("Client")
+
+                return < Marker
+                    label={isClientToShow ? { text: `${store.sites.length}`, color: 'white' } : null}
+                    // title={{ text: `${store.sites.length}`, color: 'red' }}
+                    icon={icon}
+                    key={index}
+                    id={index}
+                    position={{
+                        lat: store.lat,
+                        lng: store.lng,
+                    }}
+                    onClick={(props, marker, e) => {
+                        return !isClientToShow ? this._onMarkerClick(props, marker, e, store) : this._ClientSites(props, marker, e)
+                    }}
+                >
+                </Marker>
+            }
+
+            ) :
+
+            stores.map((store, index) => {
+               console.log({store})
+                return < Marker
+
+                    icon={icon}
+                    key={index}
+                    id={index}
+                    position={{
+                        lat: store.lat,
+                        lng: store.lng,
+                    }}
+                    onClick={(props, marker, e) => {
+                        return !isClientToShow ? this._onMarkerClick(props, marker, e, store) : this._ClientSites(props, marker, e)
+                    }}
+                >
+                </Marker>
+            }
+
+            )
     }
     _onMarkerClick = (props, marker, e, siteData) => {
         console.log({ siteData })
@@ -77,14 +115,14 @@ export class MapContainer extends Component {
             selectedPlace: props,
             activeMarker: marker,
             showingInfoWindow: true,
-            infoSiteData : siteData 
+            infoSiteData: siteData
 
         });
     };
 
     _ClientSites = (props, marker, e) => {
         const mmap = this.mapRef.current
-        mmap.map.setZoom(9.5);
+        mmap.map.setZoom(9);
         mmap.map.setCenter(marker.getPosition());
         // marker.addListener('click', function () {
         //     mmap.map.setZoom(9.5);
@@ -104,8 +142,10 @@ export class MapContainer extends Component {
     };
 
     render() {
+        // console.log(this.props.clientLists)
         const { media } = this.props
 
+        // console.log({ clientLists: this.props.clientLists })
 
         return (
             <div style={{ borderRadius: 4, border: '0.7px solid #cccccc', height: '500px', position: 'relative', bottom: '0', paddingBottom: '40%', paddingRight: '10', paddingLeft: '0%', overflow: 'hidden', margin: '0px' }}>
@@ -155,63 +195,63 @@ export default GoogleApiWrapper({
     apiKey: "AIzaSyAcWK8WHabUh0BMDZuIIPo0qfWXWarBzoo"
 })(withRouter(withMedia(MapContainer)));
 
-const clientLists1 = [
-    {
-        name: "Taiwan",
-        title: "Taiwan",
-        lat: 23.69781,
-        lng: 120.960515,
-        id: 1,
-        sites: [
-            { name: "site 1", lat: 23.345345, lng: 120.67866 },
-            { name: "site 2", lat: 23.567567, lng: 120.97866 },
-            { name: "site 3", lat: 23.57787, lng: 120.67645 },
-            { name: "site 4", lat: 23.89877, lng: 120.789786 },
+// const clientLists1 = [
+//     {
+//         name: "Taiwan",
+//         title: "Taiwan",
+//         lat: 23.69781,
+//         lng: 120.960515,
+//         id: 1,
+//         sites: [
+//             { name: "site 1", lat: 23.345345, lng: 120.67866 },
+//             { name: "site 2", lat: 23.567567, lng: 120.97866 },
+//             { name: "site 3", lat: 23.57787, lng: 120.67645 },
+//             { name: "site 4", lat: 23.89877, lng: 120.789786 },
 
-        ]
-    },
-    {
-        name: "Hong Kong",
-        title: "Hong Kong",
-        lat: 22.4433,
-        lng: 114.1234,
-        id: 2,
-        sites: [
-            { name: "site 1", lat: 22.3245, lng: 114.2313 },
-            { name: "site 2", lat: 22.6576, lng: 114.3322 },
+//         ]
+//     },
+//     {
+//         name: "Hong Kong",
+//         title: "Hong Kong",
+//         lat: 22.4433,
+//         lng: 114.1234,
+//         id: 2,
+//         sites: [
+//             { name: "site 1", lat: 22.3245, lng: 114.2313 },
+//             { name: "site 2", lat: 22.6576, lng: 114.3322 },
 
-        ]
-    },
-    {
-        name: "Taipie",
-        title: "Taipie",
-        lat: 25.03335,
-        lng: 121.56646730,
-        id: 3,
-        sites: [
-            { name: "site 1", lat: 25.0456457, lng: 121.2342342 },
-            { name: "site 2", lat: 25.0546464, lng: 121.3233333 },
-            { name: "site 3", lat: 25.06789273, lng: 121.5454544 },
-            { name: "site 4", lat: 25.073676, lng: 121.23254454 },
-            { name: "site 5", lat: 25.02323, lng: 121.43254454 },
-            { name: "site 6", lat: 25.031313, lng: 121.33254454 }
-        ]
-    },
-    {
-        name: "Xiamen",
-        title: "Xiamen",
-        lat: 24.4798,
-        lng: 118.089,
-        id: 4,
-        sites: [
-            { name: "site 1", lat: 24.5234, lng: 118.121 },
-            { name: "site 2", lat: 24.5122, lng: 118.111 },
-            { name: "site 3", lat: 24.3456, lng: 118.089 },
-            { name: "site 4", lat: 24.4123, lng: 118.099 },
-            { name: "site 4", lat: 24.473676, lng: 118.23254454 }
-        ]
-    }
-];
+//         ]
+//     },
+//     {
+//         name: "Taipie",
+//         title: "Taipie",
+//         lat: 25.03335,
+//         lng: 121.56646730,
+//         id: 3,
+//         sites: [
+//             { name: "site 1", lat: 25.0456457, lng: 121.2342342 },
+//             { name: "site 2", lat: 25.0546464, lng: 121.3233333 },
+//             { name: "site 3", lat: 25.06789273, lng: 121.5454544 },
+//             { name: "site 4", lat: 25.073676, lng: 121.23254454 },
+//             { name: "site 5", lat: 25.02323, lng: 121.43254454 },
+//             { name: "site 6", lat: 25.031313, lng: 121.33254454 }
+//         ]
+//     },
+//     {
+//         name: "Xiamen",
+//         title: "Xiamen",
+//         lat: 24.4798,
+//         lng: 118.089,
+//         id: 4,
+//         sites: [
+//             { name: "site 1", lat: 24.5234, lng: 118.121 },
+//             { name: "site 2", lat: 24.5122, lng: 118.111 },
+//             { name: "site 3", lat: 24.3456, lng: 118.089 },
+//             { name: "site 4", lat: 24.4123, lng: 118.099 },
+//             { name: "site 4", lat: 24.473676, lng: 118.23254454 }
+//         ]
+//     }
+// ];
 const Animatedicon = props => ({
     anchor: new props.google.maps.Point(30, 30),
     url: 'data:image/svg+xml;utf-8, \<svg viewBox="-5 -5 100 -100"  width="70"  height="60" xmlns="http://www.w3.org/2000/svg" version="1.1">\

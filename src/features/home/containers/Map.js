77ae32d632dfeebaf350a3a7b4fd484fbd6aa7d3
Nieruht <ethical00002020@gmail.com>
2019-09-24 09:34:39 +0630

    ChangesMap By @Thurein

--- src/features/home/containers/Map.js
+++ src/features/home/containers/Map.js
@@ -49,12 +49,14 @@ export class MapContainer extends Component {
                 return [...r, ...sitesLocs]
             }, [])
 
-            this.setState({ stores: listToShow, isClientToShow: mmap.map.zoom <= 10 && (vendor_id > -1 || this.props.clientLists.length > 1), isShowInfoWindow: this.props.clientLists.length >= 1 })
+            this.setState({ stores: listToShow, isClientToShow: mmap.map.zoom <= 10 && (vendor_id > -1 || this.props.clientLists.length >= 1), isShowInfoWindow: this.props.clientLists.length >= 1 })
         })
     }
 
     componentDidUpdate(prevProps, prevState) {
         if (prevProps.clientLists !== this.props.clientLists) {
+
+
             const { vendor_id } = this.state
             const mmap = this.mapRef.current
             const mapBound = mmap.map.getBounds()
@@ -63,7 +65,8 @@ export class MapContainer extends Component {
                 const sitesLocs = c.sites.map(v => v)
                 return [...r, ...sitesLocs]
             }, [])
-            this.setState({ showingInfoWindow: false, stores: listToShow, isClientToShow: mmap.map.zoom <= 10 && (vendor_id > -1 || this.props.clientLists.length > 1), isShowInfoWindow: this.props.clientLists.lenght >= 1 })
+            this.setState({ showingInfoWindow: false, stores: listToShow, isClientToShow: mmap.map.zoom <= 10 && (vendor_id > -1 || this.props.clientLists.length >= 1), isShowInfoWindow: this.props.clientLists.lenght >= 1 })
+            console.log(this.props.clientLists.length)
         }
     }
 
